31fbe3c6184a9a9d55d949f6b250a98a26083917
madhaviganji <madhaviganji9701@gmail.com>
2023-11-10 11:54:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def madhavi1(request):
+    return render(request,'madhavi1.html')
+def madhavi2(request):
+    return render(request,'madhavi2.html')
+def madhavi3(request):
+    return render(request,'madhavi3.html')
+def madhavi4(request):
+    return render(request,'madhavi4.html')
